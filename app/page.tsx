97d526bab479015b2338c57adc07d8ee04520d6a
Ray Orole <email@anon.co>
2024-09-02 21:10:30 +0200

    LOL

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,6 @@ import {
   FilmIcon,
 } from "lucide-react";
 
-import Navbar from "@/components/shared/navbar";
-import Footer from "@/components/shared/footer";
-
 export default function Home() {
   return (
     <div>
